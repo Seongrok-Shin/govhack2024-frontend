9e9ff9202975db4677fc7a07d4d27eca9bc450d4
AliceHeeyeon <aliceheeyeonkim@gmail.com>
2024-09-07 11:54:23 +1200

    setup SASS and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import {HashRouter, Routes, Route} from 'react-router-dom'
+
+//Pages
+import Home from './sections/Home';
+import CostCalculation from './sections/CostCalculation';
+import GetStarted from './sections/GetStarted';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <HashRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/costcalculation' element={<CostCalculation/>} />
+        <Route path='/getstarted' element={<GetStarted />} />
+      </Routes>
+     </HashRouter>
     </div>
   );
 }
